b31d2d9667c7ec37c13ffc6644a996ade6850c03
Philipp Raevskii <57037988+ArKosH-SUAI@users.noreply.github.com>
2022-02-22 18:23:21 +0300

    Delete src directory

--- src/JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Array_List"},{"p":"<Unnamed>","l":"List"},{"p":"<Unnamed>","l":"QuickSort"}];updateSearchResults();
